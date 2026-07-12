0843d1970dd5a450ced7c891a4bacfbb6056a846
BOLLAM JAYASANKAR <22l61a0507@cec.ac.in>
2026-07-12 13:18:43 +0530

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -95,7 +95,7 @@ export const updateProfile = async (req, res) => {
       return res.status(400).json({ message: "Profile pic is required" });
     }
 
-    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const uploadResponse = await cloudinary.uploader.upload(profilePic,{folder:"messaging/profilePics"});
    const updatedUser = await User.findByIdAndUpdate(
   userId,
   { profilePic: uploadResponse.secure_url },
